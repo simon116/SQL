6da0c8c96c98f64f76e1dbcd8e739953a3f51535
simon116 <50114057+simon116@users.noreply.github.com>
2019-10-29 22:44:29 -0400

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,32 @@
+-- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
+SELECT  emp.emp_no,
+        emp.last_name,
+        emp.first_name,
+        emp.gender,
+        sal.salary
+FROM employees as emp
+    LEFT JOIN salaries as sal
+    ON (emp.emp_no = sal.emp_no)
+ORDER BY emp.emp_no;
+
+-- 2. List employees who were hired in 1986.
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name, and start and end employment dates.
+
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+
+-- 5. List all employees whose first name is "Hercules" and last names begin with "B."
+
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
